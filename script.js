e18ccff2405a5d743e0e365fd7cfaedeca4ff1b4
HARSH SHUKLA <ashuklaharsh2005@gmail.com>
2023-05-13 16:36:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let userEvent = prompt("Enter event Date to CountDown to {in the format DD MMM YYYY (eg, 1 Jan 2025)}");
-var userEventName = prompt("Enter your event's name");
+let userEvent = prompt("Enter any event Date to CountDown to in the same format (dd Mmm yyyy)", '1 Jan 2025');
+let userEventName = prompt("Enter your event's name", 'Year 2025');
 
 
 const eventNameEl  = document.getElementById('bigText');
@@ -9,19 +9,19 @@ const minsEl  = document.getElementById('mins');
 const secsEl  = document.getElementById('secs');
 
 
-var upcomingEvent = userEvent;
+let upcomingEvent = userEvent;
 
 
 function timeLeft() {
-  var dateToday    = new Date();
-  var dateUpcoming = new Date(upcomingEvent);
+  let dateToday    = new Date();
+  let dateUpcoming = new Date(upcomingEvent);
    
-  var seconds= Math.floor((dateUpcoming - dateToday) / 1000);
-  var days   = Math.floor(seconds / 3600 / 24);
-  var hours  = Math.floor(seconds / 3600) % 24;
-  var mins   = Math.floor(seconds / 60) % 60;
-  var secs   = Math.floor(seconds) % 60 ;
-  var eventName = `Your event '${userEventName}' is in`
+  let seconds= Math.floor((dateUpcoming - dateToday) / 1000);
+  let days   = Math.floor(seconds / 3600 / 24);
+  let hours  = Math.floor(seconds / 3600) % 24;
+  let mins   = Math.floor(seconds / 60) % 60;
+  let secs   = Math.floor(seconds) % 60 ;
+  let eventName = `Your event '${userEventName}' is in`
 
   eventNameEl.innerHTML  = eventName;
   daysEl.innerHTML  = days;
